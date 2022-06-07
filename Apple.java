00c7151d32c1b6803660e38e9f98608b1cfed7a4
asdafwr <105795748+sasadwwdae@users.noreply.github.com>
2022-06-07 09:35:34 -0400

    today's work

--- Apple.java
+++ Apple.java
@@ -1,19 +1,33 @@
 import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 
 /**
- * Write a description of class Apple here.
+ * Write a description of class apple here.
  * 
- * @author (your name) 
- * @version (a version number or a date)
+ * @author Henry
+ * @version May 2022
  */
 public class Apple extends Actor
 {
+    int speed = 2;
     /**
-     * Act - do whatever the Apple wants to do. This method is called whenever
+     * Act - do whatever the apple wants to do. This method is called whenever
      * the 'Act' or 'Run' button gets pressed in the environment.
      */
     public void act()
     {
         // Add your action code here.
+        setLocation(getX(),getY()+speed);
+        
+        Myworld world = (Myworld) getWorld();
+        if(getY() >= world.getHeight())
+        {
+            world.gameOver();
+            world.removeObject(this);
+        }
+    }
+    
+    public void setSpeed(int spd)
+    {
+        speed = spd;
     }
 }
--- Elephant.java
+++ Elephant.java
@@ -1,4 +1,4 @@
-import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 
 /**
  * Write a description of class Elephant here.
@@ -12,9 +12,50 @@ public class Elephant extends Actor
      * Act - do whatever the Elephant wants to do. This method is called whenever
      * the 'Act' or 'Run' button gets pressed in the environment.
      */
+    
+    GreenfootSound elephantSound = new GreenfootSound("Elephant.mp3");
+    GreenfootImage idle = new GreenfootImage("images/elephant1.png");
+    
+    public Elephant()
+    {
+        for(int i = 0; i < 2; i++)
+        {
+             idle = new GreenfootImage("Images/elephant" + i + ".png");
+        }
+        setImage(idle);
+    }
+    
+    int imageIndex = 0;
+    public void animateElephant()
+    {
+        
+    }
+    
     public void act()
     {
         // Add your action code here.
+        if(Greenfoot.isKeyDown("a"))
+        {
+            move(-2);
+        }
+        else if(Greenfoot.isKeyDown("d"))
+        {
+            move(2);
+        }
         
+        eat();
+        animateElephant();
     }
+    
+    public void eat()
+        {
+            if(isTouching(Apple.class))
+            {
+                removeTouching(Apple.class);
+                Myworld world = (Myworld) getWorld();
+                world.createApple();
+                world.increaseScore();
+                elephantSound.play();
+            }
+        }   
 }
