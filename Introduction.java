32093fab9b65a84b70d1e773d916f4a4a2514d55
asdafwr <105795748+sasadwwdae@users.noreply.github.com>
2022-06-14 09:11:12 -0400

    the background change

--- Introduction.java
+++ Introduction.java
@@ -16,6 +16,18 @@ public class Introduction extends World
     public Introduction()
     {    
         // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
-        super(600, 400, 1); 
+        super(800, 600, 1,false);
+        GreenfootImage bgImage = new GreenfootImage("images/download.jpeg");
+        bgImage.scale(getWidth(), getHeight());
+        setBackground(bgImage);
+    }
+    
+    public void intro()
+    {
+        if(Greenfoot.isKeyDown("f"))
+        {
+            MyWorld gameWorld = new MyWorld();
+            Greenfoot.setWorld(gameWorld);
+        }
     }
 }
--- TheTitle.java
+++ TheTitle.java
@@ -9,6 +9,7 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 public class TheTitle extends World
 {
     Label titelLabel = new Label("Catch Apple",60);
+    Label introduction = new Label("introduction(press F)",40);
     
     /**
      * Constructor for objects of class TheTitle.
@@ -19,11 +20,26 @@ public class TheTitle extends World
         // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
         super(800, 600, 1); 
         addObject(titelLabel, getWidth()/2,getHeight()/2);
+        
+        GreenfootImage bgImage = new GreenfootImage("images/download.jpeg");
+        bgImage.scale(getWidth(), getHeight());
+        setBackground(bgImage);
+        
+        addObject(introduction, getWidth()/2,getHeight()/4*3-60);
     }
     
     public void act()
     {
         if(Greenfoot.isKeyDown("space"))
+        {
+            MyWorld gameWorld = new MyWorld();
+            Greenfoot.setWorld(gameWorld);
+        }
+    }
+    
+    public void intro()
+    {
+        if(Greenfoot.isKeyDown("f"))
         {
             MyWorld gameWorld = new MyWorld();
             Greenfoot.setWorld(gameWorld);
